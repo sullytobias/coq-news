2b7a4edfe8d303cfe1b0b9c97e03afdd83279abe
Sullivan <sullytobias@gmail.com>
2023-10-15 16:29:43 +0200

    Init QUD Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import coqBanner from "./assets/cavesOfQudBanner.png";
+
+import "./App.scss";
+
+function App() {
+    return (
+        <div className="App">
+            <img src={coqBanner} alt="coqBanner" />;
+        </div>
+    );
+}
+
+export default App;
